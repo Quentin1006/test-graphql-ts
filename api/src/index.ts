c1d92439e8f0d3cf0d6f44a37d7f7a84bb8e1103
quentin <quentin.sahal@gmail.com>
2021-03-14 11:28:56 +0100

    small fixes

--- api/src/index.ts
+++ api/src/index.ts
@@ -5,11 +5,6 @@ import app from "./app";
 import run from "./run";
 import { PORT } from "./config/app.config";
 
-let i = 1;
-setInterval(() => {
-  console.log("Init API, log number:", i++);
-}, 2000);
-
 const dbClient: IDBClient = new DBClient();
 const server = new ExpressServer(app(dbClient), { port: PORT });
 
